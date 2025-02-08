15af23c87ac3d95564fc3c7aa75e685bedbb4866
Marmik Patel <marmikp19@gmail.com>
2025-02-07 22:03:11 -0500

    add calmness

--- web/app/context/NeurosityContext.tsx
+++ web/app/context/NeurosityContext.tsx
@@ -1,22 +1,35 @@
 import { Neurosity } from "@neurosity/sdk";
 import { createContext, useContext, useEffect, useState } from "react";
 
-const NeurosityContext = createContext<Neurosity | null>(null);
+type NeurosityContextType = {
+    device: Neurosity | null;
+    calm: number;
+  };
+
+const NeurosityContext = createContext<NeurosityContextType  | null>(null);
 
 export const NeurosityProvider = ({ children }: { children: React.ReactNode }) => {
     const [device, setDevice] = useState<Neurosity | null>(null);
-    
+    const [calm, setCalm] = useState(0);
+
     useEffect(() => {
         const neurosity = new Neurosity({ 
             deviceId: "deviceId" 
         });
         
         setDevice(neurosity);
-        
+
+        const subscription = neurosity.calm().subscribe((calm) => {
+            setCalm(Number(calm.probability.toFixed(2)));
+          });
+      
+          return () => {
+            subscription.unsubscribe();
+          };
       }, []);
 
       return (
-        <NeurosityContext.Provider value={device}>
+        <NeurosityContext.Provider value={{device, calm}}>
           {children}
         </NeurosityContext.Provider>
       );
