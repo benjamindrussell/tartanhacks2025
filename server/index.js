41317473555caa8ade234507a8259a295e240cf2
Marmik Patel <marmikp19@gmail.com>
2025-02-08 00:56:08 -0500

    redo connection

--- server/index.js
+++ server/index.js
@@ -1,21 +1,40 @@
 const { Neurosity } = require("@neurosity/sdk");
-require("dotenv").config();
+require('dotenv').config();
 
 const deviceId = process.env.DEVICE_ID || "";
+const email = process.env.EMAIL || "";
+const password = process.env.PASSWORD || "";
 
-const verifyEnvs = (deviceId) => {
-    const invalidEnv = (env) => {
-      return env === "" || env === 0;
-    };
-    if (invalidEnv(deviceId)) {
-      console.error(
-        "Please verify deviceId is in .env file, quitting..."
-      );
+const verifyEnvs = (email, password, deviceId) => {
+  const invalidEnv = (env) => {
+    return (env === "");
+  }
+  if (invalidEnv(email) || invalidEnv(password) || invalidEnv(deviceId)) {
+      console.error("Please verify deviceId, email and password are in .env file, quitting...");
       process.exit(0);
-    }
-  };
-  verifyEnvs(deviceId);
+  }
+}
+verifyEnvs(email, password, deviceId);
+console.log(`${email} attempting to authenticate with ${deviceId}`);
 
-  const neurosity = new Neurosity({
-    deviceId
+const neurosity = new Neurosity({
+  deviceId
+});
+
+const main = async () => {
+  await neurosity.login({
+    email,
+    password
+  })
+  .catch(error => {
+    console.log(error);
+    throw new Error(error);
   });
+  console.log("Logged in");
+
+  neurosity.focus().subscribe((focus) => {
+      console.log(focus.probability);
+  });
+}
+
+main();
