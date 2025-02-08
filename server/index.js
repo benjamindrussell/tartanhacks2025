50d371a9914b53addf81c1b41d50f48571753bd5
Marmik Patel <marmikp19@gmail.com>
2025-02-08 00:17:01 -0500

    add env variable check

--- server/index.js
+++ server/index.js
@@ -1,2 +1,21 @@
 const { Neurosity } = require("@neurosity/sdk");
 require("dotenv").config();
+
+const deviceId = process.env.DEVICE_ID || "";
+
+const verifyEnvs = (deviceId) => {
+    const invalidEnv = (env) => {
+      return env === "" || env === 0;
+    };
+    if (invalidEnv(deviceId)) {
+      console.error(
+        "Please verify deviceId is in .env file, quitting..."
+      );
+      process.exit(0);
+    }
+  };
+  verifyEnvs(deviceId);
+
+  const neurosity = new Neurosity({
+    deviceId
+  });
